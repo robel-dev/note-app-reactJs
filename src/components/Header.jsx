b56f4e9e98a8292a0d75a095de5196c6cc36bac6
robel-dev <robelamare20@gmail.com>
2021-11-29 18:00:11 +0300

    Note app using Reactjs

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CreateRoundedIcon from '@material-ui/icons/CreateRounded';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <CreateRoundedIcon />
+        My Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
